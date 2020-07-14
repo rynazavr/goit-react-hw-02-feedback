435d7cc528eaee293bd26b1ca137767a462f19c0
rynazavr <lunevaira@gmail.com>
2020-07-14 16:20:38 +0300

    Updates

---
+++ precache-manifest.ebd101d47f9a68b7ff4263ee68ff372e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8600248dbc74f5f49c7f04c91fd7208",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "91e1e9aec4058e253326",
+    "url": "/goit-react-hw-02-feedback/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9b2bf4c9921ced580578",
+    "url": "/goit-react-hw-02-feedback/static/js/2.d6ce7a72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.d6ce7a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91e1e9aec4058e253326",
+    "url": "/goit-react-hw-02-feedback/static/js/main.fa776c16.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
